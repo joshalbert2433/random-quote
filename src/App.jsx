8eaa359fbf08ba93f56af20259eb153462a40498
Joshua Albert Villanueva <joshua.villanueva@paychat.ph>
2023-11-07 17:22:51 +0800

    add error 404 page not found page

--- src/App.jsx
+++ src/App.jsx
@@ -7,51 +7,52 @@ import RandomQuotes from "./pages/RandomQuotes";
 import AuthorQuotes from "./pages/AuthorQuotes";
 
 function App() {
-	const location = useLocation();
-	const navigate = useNavigate();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-	const isAuthorRouteActive = location.pathname.includes("author")
-		? true
-		: false;
+  const isAuthorRouteActive = location.pathname.includes("author")
+    ? true
+    : false;
 
-	const { refetch } = useRandomQuotes();
+  const { refetch } = useRandomQuotes();
 
-	return (
-		<>
-			<div className="mx-24 py-6 min-h-screen flex flex-col">
-				<div className="flex justify-end cursor-pointer hover:text-[#a78e44] ">
-					<div
-						className="flex gap-2 items-center parent-custom"
-						onClick={() =>
-							!isAuthorRouteActive
-								? refetch()
-								: navigate({ pathname: "/" })
-						}
-					>
-						random{" "}
-						<BsArrowRepeat size={24} className="hover-custom" />
-					</div>
-				</div>
+  return (
+    <>
+      <div className="mx-24 py-6 min-h-screen flex flex-col">
+        <div className="flex justify-end cursor-pointer hover:text-[#a78e44] ">
+          <div
+            className="flex gap-2 items-center parent-custom"
+            onClick={() =>
+              !isAuthorRouteActive ? refetch() : navigate({ pathname: "/" })
+            }
+          >
+            random <BsArrowRepeat size={24} className="hover-custom" />
+          </div>
+        </div>
 
-				<main className="grow max-w-[614px] mx-auto flex flex-col items-center justify-center">
-					<Routes>
-						<Route path="/" element={<RandomQuotes />} />
-						<Route
-							path="/author/:name"
-							element={<AuthorQuotes />}
-						/>
-					</Routes>
-				</main>
-				<footer className="self-center mt-24">
-					<p className="montserrat-custom text-[#828282]">
-						created by{" "}
-						<span className="font-semibold">joshalbert2433</span> -
-						devChallenges.io
-					</p>
-				</footer>
-			</div>
-		</>
-	);
+        <main className="grow max-w-[614px] mx-auto flex flex-col items-center justify-center">
+          <Routes>
+            <Route path="/" element={<RandomQuotes />} />
+            <Route path="/author/:name" element={<AuthorQuotes />} />
+            <Route
+              path="*"
+              element={
+                <div className="text-4xl font-bold min-h-screen flex items-center justify-center">
+                  Page not found
+                </div>
+              }
+            />
+          </Routes>
+        </main>
+        <footer className="self-center mt-24">
+          <p className="montserrat-custom text-[#828282]">
+            created by <span className="font-semibold">joshalbert2433</span> -
+            devChallenges.io
+          </p>
+        </footer>
+      </div>
+    </>
+  );
 }
 
 export default App;
